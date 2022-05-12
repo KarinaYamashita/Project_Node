792420a8e1e37a66d841311a2fe557750ee96128
Karina Yamashita <karina.hoshi@gmail.com>
2022-05-12 17:38:06 -0300

    usando express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('olá mundo');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Olá mundo pelo Express'))
+
+app.listen(port, ()=> console.log('Api rodando na porta 3000'))
+
